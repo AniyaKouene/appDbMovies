10c8247678d26a9870a0c6c4eec84e6c348e486b
Aniya Kouene <aniya.kouene@gmail.com>
2018-09-24 08:21:18 +0200

    first commit

---
+++ src/containers/VideoList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import VideoListItem from "../components/VideoListItem";
+
+const VideoList = () => {
+    const movies = ["film1", "film2", "film3", "film4", "film5",]
+  return (
+    <div>
+        <ul>
+        <VideoListItem movie = {movies[0]}/>
+        <VideoListItem movie = {movies[1]}/>
+        <VideoListItem movie = {movies[2]}/>
+        <VideoListItem movie = {movies[3]}/>
+        <VideoListItem movie = {movies[4]}/>
+        </ul>
+    </div>
+  );
+};
+
+export default VideoList;
